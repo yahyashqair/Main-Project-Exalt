fb4e4ac01fc3570399fd373347534e3fecad704d
yahyashqair <37252929+yahyashqair@users.noreply.github.com>
2019-08-25 15:49:40 +0300

    Add Server Controller

---
+++ src/main/java/com/exult/ProjectCisco/controller/ServerController.java
@@ -0,0 +1,32 @@
+package com.exult.ProjectCisco.controller;
+
+import com.exult.ProjectCisco.model.Server;
+import com.exult.ProjectCisco.service.server.ServerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@Controller
+@RestController
+@RequestMapping("/server")
+public class ServerController {
+
+    @Autowired
+    private ServerService serverService;
+
+    @RequestMapping(value = "/all", method = RequestMethod.GET)
+    public List<Server> getAllServers() {
+        return serverService.getAll();
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public Server getServer(@PathVariable("id") Long id) {
+        return serverService.getServer(id);
+    }
+
+}
